6071cd54b2e2d558166cae026b114c44340cda52
rehanzhaky <hannn9491@gmail.com>
2025-03-23 21:05:04 +0700

    mod: Add Class Component

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Image, Text, TextInput, View} from 'react-native';
 
 // Functional Component
@@ -14,11 +14,14 @@ const App = () => {
       <ImplementasiImage />
       {/* Pemanggilan Functional Component Implementasi Text Input */}
       <ImplementasiTextInput />
+      {/* Pemanggilan Class Component, Implementasi Class Component */}
+      <PhotoProfile />
     </View>
   );
 };
 
 // Buat functional component baru, implementasi Text
+// Bisa dengan shortcut 'rfce'
 const ImplementasiText = () => {
   return <Text>Ini adalah Implementasi Text</Text>;
 };
@@ -48,5 +51,26 @@ const ImplementasiImage = () => {
 const ImplementasiTextInput = () => {
   return <TextInput style={{borderWidth: 1}} />;
 };
+
+// Buat class component baru, class photo profile berisi image dan text
+// Bisa dengan shortcut 'cc'
+class PhotoProfile extends Component {
+  render() {
+    return (
+      <View>
+        <Image
+          source={{
+            uri: 'https://images.unsplash.com/photo-1726064855955-5fe51cd6993c?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+          }}
+          style={{width: 100, height: 100, borderRadius: 100}}
+        />
+
+        <Text style={{color: 'red', fontSize: 24, fontWeight: '600'}}>
+          Ini adalah Photo Profile
+        </Text>
+      </View>
+    );
+  }
+}
 
 export default App;
